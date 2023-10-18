327f4a3777c6b8b3b7245ca7f90c2980dba607a2
Tanya <tanya.yu.job@gmail.com>
2023-10-18 23:12:38 +0300

    add try-catch for ReadInputData method

--- PSB_Test_Assignment/InputDataParser.cs
+++ PSB_Test_Assignment/InputDataParser.cs
@@ -5,60 +5,71 @@ namespace PSB_Test_Assignment
     public class InputDataParser
     {
         public static string? ReadInputData(string path)
-        {                            
+        {
             try
             {
-                if (!File.Exists(path))
-                {
-                    throw new NullReferenceException("File not found!");
-                }
-                else
+                if (File.Exists(path))
                 {
                     return File.ReadAllText(path, Encoding.UTF8);
                 }
+
+                Console.WriteLine("File not found!");
             }
-            catch (NullReferenceException e)
+
+            catch (Exception e)
             {
                 Console.WriteLine($"Exception: {e.Message}");
             }
 
-            Console.ReadLine();
             return null;
         }
 
-        public List<MatchResults> ParseInputData(string data)
+        public List<MatchResults> ParseInputData(string? data)
         {
             var results = new List<MatchResults>();
-            var stringResult = data.Split(",");
-            foreach (var matchResult in stringResult)
+
+            try
             {
-                var resultWithoutQuotes = matchResult.Replace("\"", "");
-                var parsedResult = resultWithoutQuotes.Split(":");
-
-                if (parsedResult.Length != 2) 
+                if (string.IsNullOrEmpty(data))
                 {
-                    Console.WriteLine($"Invalid match result (!= 2): {resultWithoutQuotes}");
-                    continue;
+                    throw new Exception("Input data is null or empty.");
                 }
 
-                if (int.TryParse(parsedResult[0], out var teamOne) && int.TryParse(parsedResult[1], out var teamTwo))
+                var stringResult = data.Split(",");
+                foreach (var matchResult in stringResult)
                 {
-                    if ((teamOne >= 0) && (teamTwo >= 0))
-                    {                      
-                        var match = new MatchResults(teamOne, teamTwo);
-                        results.Add(match);
+                    var resultWithoutQuotes = matchResult.Replace("\"", "");
+                    var parsedResult = resultWithoutQuotes.Split(":");
+
+                    if (parsedResult.Length != 2)
+                    {
+                        Console.WriteLine($"Invalid match result (!= 2): {resultWithoutQuotes}");
+                        continue;
                     }
-                }
 
-                else
-                {
-                    Console.WriteLine($"Invalid match result (not integer or negative): {resultWithoutQuotes}");
+                    if (int.TryParse(parsedResult[0], out var teamOne) && int.TryParse(parsedResult[1], out var teamTwo))
+                    {
+                        if ((teamOne >= 0) && (teamTwo >= 0))
+                        {
+                            var match = new MatchResults(teamOne, teamTwo);
+                            results.Add(match);
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"Invalid match result (not integer or negative): {resultWithoutQuotes}");
+                    }
+
                 }
-             
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
             return results;
         }
-
     }
 }
--- PSB_Test_Assignment/Program.cs
+++ PSB_Test_Assignment/Program.cs
@@ -1,10 +1,11 @@
 ﻿using PSB_Test_Assignment;
 
 var parser = new InputDataParser();
+var calculator = new ScoreCalculation();
+
 var data = InputDataParser.ReadInputData(@"Match Results\matchResults.txt");
 var results = parser.ParseInputData(data);
 
-var calculator = new ScoreCalculation();
 (int teamOneScore, int teamTwoScore) = calculator.CalculateScore(results);
 
 Console.WriteLine($"Final results: Team One = {teamOneScore}, Team Two = {teamTwoScore}");
